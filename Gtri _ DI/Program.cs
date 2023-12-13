3bcc939c7366d79220aa43f60e56f2afd726de99
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-13 21:45:11 +0530

    Add project files.

---
+++ Gtri _ DI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getri_DI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer(new Processor(10));
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Computer
+    {
+        Processor processor;
+        public Computer(Processor _processor)
+        {
+            processor = _processor;
+            Console.WriteLine("Computer Class Called");
+        }        
+    }
+
+    public class Processor
+    {
+        public Processor(int speed)
+        {
+            Console.WriteLine($"{speed}");
+            Console.WriteLine("Processor Class Called");
+        }
+    }
+}
